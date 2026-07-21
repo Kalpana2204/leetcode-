ff9d2b53711bdbfd8510aca593b3b90776e97ddf
KALPANA MADIWALAPPA NEGINAL <148645688+Kalpana2204@users.noreply.github.com>
2026-07-21 10:25:57 +0530

    Time: 1 ms (17.05%), Space: 17.4 MB (78.89%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool dfs(int node, int color, vector<int>& vis, vector<vector<int>>& graph) {
         vis[node] = color;
-
         for (int neigh : graph[node]) {
             if (vis[neigh] == 0) {
                 if (!dfs(neigh, 3 - color, vis, graph))
@@ -11,21 +10,17 @@ public:
             else if (vis[neigh] == color)
                 return false;
         }
-
         return true;
     }
-
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> vis(n, 0);
-
         for (int i = 0; i < n; i++) {
             if (vis[i] == 0) {
                 if (!dfs(i, 1, vis, graph))
                     return false;
             }
         }
-
         return true;
     }
 };
