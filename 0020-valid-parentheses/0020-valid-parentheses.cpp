f81bd5570d2879205c79dea363f96c044ea19ce1
KALPANA MADIWALAPPA NEGINAL <148645688+Kalpana2204@users.noreply.github.com>
2026-08-30 17:07:45 +0530

    Time: 0 ms (100%), Space: 9 MB (37.37%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int n=s.size();
+        for(int i=0; i<n; i++){
+            if(s[i]=='(' ||s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.size()==0) return 0;
+                if(st.top()=='(' && s[i]==')'|| st.top()=='[' && s[i]==']' || st.top()=='{' && s[i]=='}'){
+                    st.pop();
+                } 
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.size()==0;
+        
+    }
+};
